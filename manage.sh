3d9227a574fac61f908fcbc2ba845882a269cff8
Luke <lsh-0@users.noreply.github.com>
2023-06-06 11:40:05 +0930

    manage.sh, 'release' command now trims paths to local source files

--- manage.sh
+++ manage.sh
@@ -28,13 +28,18 @@ elif test "$cmd" = "release"; then
     # CGO_ENABLED=0 skips CGO and linking against glibc to build static binaries.
     # ld -s is 'disable symbol table'
     # ld -w is 'disable DWARF generation'
+    # -trimpath removes leading paths to source files
     # -v 'verbose'
     # -o 'output'
     GOOS=linux CGO_ENABLED=0 go build \
         -ldflags="-s -w" \
+        -trimpath \
         -v \
         -o linux-amd64
     sha256sum linux-amd64 > linux-amd64.sha256
+    echo ---
+    echo "wrote linux-amd64"
+    echo "wrote linux-amd64.sha256"
     exit 0
 
 elif test "$cmd" = "update-deps"; then
